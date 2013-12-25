dc410c62a1a06442dabe2b456975422cd2bbd621
Ravi Suhag <suhag.ravi@gmail.com>
2013-12-25 16:22:39 +0530

    Applying angular routing

--- app/app.js
+++ app/app.js
@@ -1,8 +1,24 @@
-bigrock = angular.module('BigRock', []);
+var bigrock = angular.module('BigRock', ['ngRoute']);
 
 bigrock.controller('homeCtrl', ['$scope',
   function($scope) {
 
-		$scope.welcome= "Hello to Big Rock";
+    $scope.welcome = "Hello to Big Rock";
+
+}]);
+
+bigrock.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(false);
+
+    $routeProvider.
+    when('/', {
+      templateUrl: 'app/views/home.html',
+      controller: 'homeCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
 
 }]);
